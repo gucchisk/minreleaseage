94ab52f361e91d70cb35fbc0d8b0f3adaf6efd15
gucchisk <gucchi_sk@yahoo.co.jp>
2026-04-26 19:50:51 +0900

    .

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Package {
-    name: string;
-    version: string;
-}
-export declare function fetchReleaseDate(packageName: string, version: string): Promise<Date>;
-export declare function readPackageLock(lockfilePath: string): Package[];
-export declare function readYarnLock(lockfilePath: string): Package[];
-export declare function readPnpmLock(lockfilePath: string): Package[];
-export declare function checkPackageAges(minAgeHours: number): Promise<void>;
